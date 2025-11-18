70c983d003c02a5cca2c4e39bba7e4b4aa963439
Andrey-Shelgunov <shelgunova3@gmail.com>
2025-11-18 09:38:05 +0300

    Добавил библиотеку классов,форму и заполнил форму и классы в библиотеке.

---
+++ Library/ReturnRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class ReturnRequest
+    {
+        public string OrderNumber { get; set; } = string.Empty;
+        public List<int> ProductIds { get; set; } = new List<int>();
+        public string Reason { get; set; } = string.Empty;
+        public string CustomerName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
